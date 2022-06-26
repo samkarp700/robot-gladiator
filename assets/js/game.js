1d1c48c6d59313563a39168d19d4ffb2815e8d93
Sam Karp <sfcypret@gmail.com>
2022-06-26 13:42:18 -0400

    store feature added integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -171,24 +171,25 @@ var fight = function(enemy) {
         var shop = function() {
             //ask the player what they'd like to do
             var shopOptionPrompt = window.prompt(
-                "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
+                "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1: REFILL, 2 for UPGRADE, OR 3 for LEAVE."
             );
+            
+            shopOptionPrompt = parseInt(shopOptionPrompt);
         //use switch to carry out action
                 switch (shopOptionPrompt) {
-                    case "REFILL": //new case
-                    case "refill" :
-                        playerInfoInfo.refillHealth();
+                    case 1:
+                        playerInfo.refillHealth();
                         break;
             
-                    case "UPGRADE": //new case
-                    case "upgrade":
+                    case 2: 
                        playerInfo.upgradeAttack();
                     break;
-                    case "LEAVE": //new case
-                    case "leave":
+
+                    case 3: 
                         window.alert("Leaving the store.");
                         //do nothing, so function will end
                         break;
+
                     default: 
                         window.alert("You did not pick a valid option. Try again.");
                        //call shop() again to force player to pick a valid option
